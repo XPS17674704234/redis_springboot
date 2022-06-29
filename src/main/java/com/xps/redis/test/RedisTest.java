f85f1516eda14128a3a86969b41c7f4a1943ed27
xiepengsheng <xiepengsheng@163.com>
2022-06-29 16:02:50 +0800

    second commit three

--- src/main/java/com/xps/redis/test/RedisTest.java
+++ src/main/java/com/xps/redis/test/RedisTest.java
@@ -5,5 +5,6 @@ public class RedisTest {
     public static void main(String[] args) {
         System.out.println("git测试");
         System.out.println("git测试2");
+        System.out.println("git测试3");
     }
 }
